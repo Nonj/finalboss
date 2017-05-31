8624e0e88f84402d1c7e1338e6dc4bae437d47f3
Nattanon Bunyatipanon <nattanon33@yahoo.com>
2017-05-30 22:16:24 -0700

    Close #5 new data

---
+++ AIDS/ui.R
@@ -0,0 +1,72 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(dplyr)
+library(plotly)
+library(rsconnect)
+library(shinythemes)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Seattle Police Department Data"),
+  
+  
+  navbarPage(
+    tabPanel(
+      "Overview",
+      
+      titlePanel(img(
+        #change the png and etc.
+        src = 'image.png',
+        width: '100px',
+        height = '100px'
+        
+      )),
+      #Add once decided on data
+      sidebarLayout(
+        sidebarPanel()
+      ),
+      mainPanel()
+      
+    ),
+    tabPanel(
+      "Crime Concentration by Location",
+      titlePanel(),
+      sidebarLayout(
+        sidebarPanel()
+      ),
+      mainPanel()
+    ),
+    tabPanel(
+      "Crime Concentration by Date/Time ",
+      titlePanel(),
+      sidebarLayout(
+        sidebarPanel()
+      ),
+      mainPanel()
+      
+    ),
+    tabPanel(
+      "LMAOOOAAAAOAOAOAOAO",
+      titlePanel(),
+      sidebarLayout(
+        sidebarPanel()
+      ),
+      mainPanel()
+    )
+    
+    
+    
+    
+  )
+  
+))
