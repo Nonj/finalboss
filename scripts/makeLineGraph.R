8d8b35979b8e37bbfb378f344405145926d7ed66
non <non@b.com>
2017-05-31 21:57:28 -0700

    close #4

--- scripts/makeLineGraph.R
+++ scripts/makeLineGraph.R
@@ -1,4 +1,7 @@
-makeLineGraph <- function(aids.data) {
-  
+# SCript to make interactive line plot
+makeLineGraph <- function(aids.data, user.country) {
+  country.data <- aids.data <- filter(Country == user.country) 
+  data.2000 <- country.data <- 
+  p <- plot_ly(data, x = ~x, y = ~random_y, type = 'scatter', mode = 'lines')
   
 }
