991b69bea94b2d124f56d881f5186b2dbd9f3ad8
Evan Zhao <evanzhao@Evans-MacBook-Pro.local>
2017-06-01 20:54:21 -0700

    added comments to scatter plot and pie chart scripts

--- scripts/makePieChart.R
+++ scripts/makePieChart.R
@@ -1,7 +1,11 @@
 makePieChart <- function(year.var){
-  library(lazyeval)
+  
+  #sorting data from most deaths to least deaths in the inputted year
   data <- arrange_(fixed.deaths, interp(~-name, name = as.name(paste0("X",year.var))))
+  #grabbing top 10 most deaths
   data <- data[1:10,]
+  
+  #making pie chart of top 10 mortality countries with hover information
   p <- plot_ly(data, labels = ~Country, values = ~eval(parse(text = paste0("X",year.var))), type = 'pie',
            textposition = 'inside',
            textinfo = 'label',
@@ -12,7 +16,7 @@ makePieChart <- function(year.var){
                          line = list(color = '#FFFFFF', width = 1)),
            showlegend = FALSE,
            pull = .2) %>%
-    layout(title = 'HIV Death Pie Chart',
+       layout(title = 'HIV Death Pie Chart',
            xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
            yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
   p
--- scripts/makeScatterPlot.R
+++ scripts/makeScatterPlot.R
@@ -1,8 +1,11 @@
 # Scatter plot
 makeScatterPlot <- function(year){
-  colname = paste0("X",year)
+  
+  #filtering out all no data empty values
   data <- filter_(fixed.prevalence, paste0("X", year ,"> 0"))
-  p <- plot_ly(data, x = ~eval(parse(text = colname)), y = ~GDP..BILLIONS.,
+  
+  #making scatter plot comparing GDP and percent infected by country
+  p <- plot_ly(data, x = ~eval(parse(text = paste0("X",year))), y = ~GDP..BILLIONS.,
           size = fixed.new$year.to.inquire, text = ~paste("Country: ", Country)) %>% 
        layout(xaxis = list(title = "Percent Infected of Total Population"), yaxis = list(title = "GDP"))
   p
